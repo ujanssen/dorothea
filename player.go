e9625917f3d0643d9f85422c29ecd29f7a41572e
Uwe Janssen <mail@uwe-janssen.de>
2014-05-31 10:06:57 +0200

    add Position and HomeArea to Player

--- player.go
+++ player.go
@@ -3,14 +3,16 @@ package dorothea
 import "math/rand"
 
 type Player struct {
-	Color   Color
-	OutArea int
+	Color    Color
+	OutArea  int
+	Position []int  // of pieces on board
+	HomeArea []bool // piece in home[0,1,2,3] ?
 }
 
 func createPlayers(playerColors []Color) []Player {
 	players := make([]Player, len(playerColors))
 	for i, _ := range players {
-		players[i] = Player{OutArea: 4, Color: playerColors[i]}
+		players[i] = Player{OutArea: 4, Color: playerColors[i], Position: make([]int, 0), HomeArea: make([]bool, 4)}
 	}
 	return players
 }
