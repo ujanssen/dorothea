fcc5b1668029e7939c96bbcdf59b25abd8a58bd6
Uwe Janssen <mail@uwe-janssen.de>
2014-06-29 12:18:00 +0200

    new Position type for Pins

--- pin.go
+++ pin.go
@@ -1,18 +1,30 @@
 package dorothea
 
+type Position int
+
+const (
+	Out     Position = iota // Pin in OutArea
+	OnField                 // Pin on Field
+	InHome                  // Pin in HomeArea
+)
+
 type Pin struct {
-	player *Player
-	field  int
+	player    *Player
+	position  Position
+	iField    int // index in game.fields when position == OnField
+	iHomeArea int // index in player.HomeArea when position == InHome
 }
 
-const OUT_AREA = -1
+const NOT_IN_INDEX = -1
 
 func NewPins(p *Player) [4]*Pin {
 	var pins [4]*Pin
 	for i := 0; i < 4; i++ {
 		pins[i] = &Pin{
-			player: p,
-			field:  OUT_AREA}
+			player:    p,
+			position:  Out,
+			iField:    NOT_IN_INDEX,
+			iHomeArea: NOT_IN_INDEX}
 	}
 	return pins
 }
